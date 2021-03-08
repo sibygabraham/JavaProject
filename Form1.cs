ceeb9a42905624e8f400a4c92d973f27516cc6d5
hello <hello@LAPTOP-NA2F1EVT>
2021-03-08 12:55:05 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace JavaProject
+{
+    [ComVisible (true)]
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+            this.webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+
+            webBrowser1.ObjectForScripting = this;
+            webBrowser1.ScriptErrorsSuppressed = false;
+
+            webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            webBrowser1.AllowWebBrowserDrop = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string CurrentDirectory = Directory.GetCurrentDirectory();
+            webBrowser1.Navigate(Path.Combine(CurrentDirectory, "HTMLPageForJavaScript.html"));
+        }
+
+        private void Report()
+        {
+            HtmlElement div = webBrowser1.Document.GetElementById("reportContent");
+            StringBuilder sb = new StringBuilder();
+            sb.Append("table");
+            sb.Append("<tr><tr><B> Hi this ismy reoprt demo</B><td></tr>");
+            sb.Append("</table");
+            div.InnerHtml = sb.ToString();
+        }
+            
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.Focus();
+            Report();
+        }
+        public void PrintReport()
+        {
+            DialogResult dr = printDialog1.ShowDialog();
+            if (dr.ToString() == "OK")
+            {
+                webBrowser1.Print();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+}
